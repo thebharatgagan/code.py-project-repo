90ac2eb324fbf0907c1c791225085d801071cf42
Bharat Gagan <bharatgagan07@gmail.com>
2026-01-05 00:09:51 +0530

    carspeedlimit.py

---
+++ carspeed.py
@@ -0,0 +1,12 @@
+carspeed = int(input("Enter the Car Speed: "))
+print("your speed =", carspeed, "km/hr")
+if(carspeed == 50):
+    print("\nyou are in limit, safe drive and follow the rules, as per given.\n")
+elif(carspeed == 70):
+    print("\nyou are in limit, slow and safe drive.\n")
+elif(carspeed == 100):
+    print("\nyou are cross the limit, but drive safe and no challan.\n")
+elif(carspeed == 150/200):
+    print("\nyou are cross the limit , and challan is exist.\n")
+    
+# End the Program.
